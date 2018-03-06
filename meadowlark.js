6a67c8372d02ab14e2c49232831f61464160d64b
ozto <1240706516@qq.com>
2018-03-06 16:58:52 +0800

    添加测试文件

--- meadowlark.js
+++ meadowlark.js
@@ -15,12 +15,36 @@ app.set('port', process.env.PORT || 3000);
 
 app.use(express.static(__dirname + '/public'));
 
+app.use(function(req, res, next) {
+	res.locals.showTests = app.get('env') !== 'production' &&
+		req.query.test === '1';
+		next();
+});
+
 app.get('/', function(req, res) {
 	res.render('home');
 });
 
+app.get('/headers', function(req,res){
+res.set('Content-Type','text/plain');
+var s = '';
+for(var name in req.headers) s += name + ': ' + req.headers[name] + '\n';
+res.send(s);
+});
+
 app.get('/about', function(req, res) {
-	res.render('about', {fortune: fortune.getFortune()});
+	res.render('about', {
+		fortune: fortune.getFortune(),
+		pageTestScript: '/qa/tests-about.js'
+	});
+});
+
+app.get('/tours/hood-river', function(req, res){
+	res.render('tours/hood-river');
+});
+
+app.get('/tours/request-group-rate', function(req, res){
+	res.render('tours/request-group-rate');
 });
 
 // 定制404页面
@@ -39,4 +63,5 @@ app.use(function(err, req, res, next) {
 app.listen(app.get('port'), function() {
 	console.log('Express started on http://localhost: ' + app.get('port'));
 	console.log('Press Ctrl + C to stop server');
-})
+});
+
